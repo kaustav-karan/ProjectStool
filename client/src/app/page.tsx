d9cd9eccc6ec150b409fa265bf1def97e4d03d25
Cebasture <manav.rohatgi22@st.niituniversity.in>
2025-02-28 13:29:07 +0530

    test client is ready

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import SearchFile from "./components/search";
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <SearchFile />
+    </div>
+  );
+}
